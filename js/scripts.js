9686da84e012b02523c40f9a1cfc6d2e8130145c
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2013-01-15 17:31:34 +0100

    fetches and renders comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,105 @@
 $(function(){
 
+  var posts = [];
 
+  function Post(postdata){
+    this.title = postdata.title;
+    this.thumbnail = postdata.thumbnail;
+
+    this.data = postdata;
+
+    posts.push(this);
+    this.render();
+  }
+  Post.prototype.render = function() {
+    var p = this;
+
+    var post = document.createElement('article');
+    post.className = 'post';
+
+    var title = document.createElement('h1');
+    title.innerHTML = this.title;
+
+    var thumbnail = document.createElement('img');
+    thumbnail.src = this.thumbnail;
+
+    post.appendChild(thumbnail);
+    post.appendChild(title);
+
+    document.getElementById('posts').appendChild(post);
+
+
+
+    $(post).click(function(){
+      renderComments(p);
+    });
+
+  }
+  Post.prototype.destroy = function() {
+    // TODO
+  }
+
+
+  var comments = [];
+  function Comment(data){
+    this.data = data;
+    comments.push(this);
+    this.render();
+  }
+  Comment.prototype.render = function() {
+    var comment = document.createElement('article');
+    var author = document.createElement('p');
+    var body = document.createElement('p');
+
+    author.innerHTML = this.data.author;
+    author.className = 'author';
+    body.innerHTML = this.data.body;
+    body.className = 'body';
+
+    comment.appendChild(author);
+    comment.appendChild(body);
+
+    comment.className = 'comment';
+
+    document.getElementById('comments-dest').appendChild(comment); 
+  }
+
+
+
+  function renderComments(post){
+    var header = document.createElement('header');
+    var title = document.createElement('h1');
+    header.appendChild(title);
+    title.innerHTML = post.data.title;
+
+    document.getElementById('comments').appendChild(header);
+
+    var dest = document.createElement('div');
+    dest.id = 'comments-dest';
+    dest.innerHTML = 'fetching comments ...';
+
+    document.getElementById('comments').appendChild(dest);
+
+    $.getJSON('http://www.reddit.com/' + post.data.permalink + '.json?jsonp=?',
+      function(data){
+        //first element contains the post data
+        //second element contains the actual comments
+
+        document.getElementById('comments-dest').innerHTML = ''; 
+
+        data[1].data.children.forEach(function(element){
+          new Comment(element.data);
+        });
+      }
+    );
+  }
 
   $.getJSON('http://www.reddit.com/r/pics.json?jsonp=?',
     function(data){
       console.log(data);
 
       data.data.children.forEach(function(element){
-        var img = document.createElement('img');
-        img.src = element.data.thumbnail;
-
-        document.getElementById('dest').appendChild(img);
+        new Post(element.data);
       });
     }
   );
